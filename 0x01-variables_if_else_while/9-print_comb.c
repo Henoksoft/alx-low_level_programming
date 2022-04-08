a48a3c3e40eb07b2643ce68f50c71677f1721e0e
henok <henokgirmajifar@yahoo.com>
2022-04-08 05:57:08 -0700

    COMB

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry poit
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		putchar(number);
+
+		if (number == 57)
+		{break;
+		}
+
+		putchar(',');
+		putchar (' ');
+	}
+	putchar('\n';
+			return (0);
+			}
