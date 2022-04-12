6cb77e310f215d2eabbe10213a7cfb6de83e9b99
henok <henokgirmajifar@yahoo.com>
2022-04-12 12:50:33 -0700

    24 hours task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_baver - prints every minute of the day
  *
  * Return: Always 0.
  */
 
-void jack_bauer(void)
+void jack_baver(void)
 
 {
 	int a;
@@ -27,5 +27,6 @@ void jack_bauer(void)
 
 
 	}
+
 
 }
