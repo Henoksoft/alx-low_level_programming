a9d8634956cf62bf6df422797f06de19ba6b122e
henok <henokgirmajifar@yahoo.com>
2022-04-12 13:19:12 -0700

    add function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * jack_baver - prints every minute of the day
+ * jack_bauer - prints every minute of the day
  *
  * Return: Always 0.
  */
 
-void jack_baver(void)
+void jack_bauer(void)
 
 {
 	int a;
