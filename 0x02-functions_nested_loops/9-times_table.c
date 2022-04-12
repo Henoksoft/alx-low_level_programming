89bfab7fdb1ed4390f01b1666bfebe3e2733abe1
henok <henokgirmajifar@yahoo.com>
2022-04-12 13:00:34 -0700

    Times table task nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times)table - ptints tables
+ * times_table - ptints tables
  *
  * Return: Always 0.
  */
@@ -9,6 +9,7 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -44,4 +45,5 @@ void times_table(void)
 		}
 		_putchar ('\n');
 	}
+
 }
