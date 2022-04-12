e30c34ce918e7e23e4e30485c73a8b509829d81d
henok <henokgirmajifar@yahoo.com>
2022-04-12 09:07:55 -0700

    except last task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add 0 prints add two numbers
+ * @i: print int i
+ * @k: print int k
+ * Retutn: Always 0.
+ */
+
+int add(int i, int k)
+{
+	return (i + K);
+}
