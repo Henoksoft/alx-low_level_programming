c6cfb7bcd0314dc86b0b291776dd3995f6cfd6cf
henok <henokgirmajifar@yahoo.com>
2022-04-14 03:35:17 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,24 @@
 
 int largeest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		lartest = c;
-	}
-	
-	return (largest);
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
+{
+lartest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
 }
