97a589604e1641cd727f2881b15242052106535d
henok <henokgirmajifar@yahoo.com>
2022-04-15 04:18:42 -0700

    101 last commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,21 +5,20 @@
  * 
  * Return: Always 0.
  */ 
-
 int main(void)
 {
-long x, y;
-
-x = 612852475143;
-
-for (y = 2; x > y; y++)
-{
-	while (n % i == 0)
+	long x, y;
+	
+	x = 612852475143;
+	
+	for (y = 2; x > y; y++)
 	{
-		x = x / y;
+		while (x % y == 0)
+		{
+			x = x / y;
+		}
 	}
-}
-printf("%lu", y);
-putchar('\n');
-return (0);
+	printf("%lu", y);
+	putchar('\n');
+	return (0);
 }
