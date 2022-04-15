75c7aecd0c09c098f3a2e64412eaaad0c1c11a12
henok <henokgirmajifar@yahoo.com>
2022-04-15 05:14:03 -0700

    Task 11 editing

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,15 +2,15 @@
 
 /**
  * main - checks for checks for a digit (0 through 9).
- * 
+ *
  * Return: Always 0.
- */ 
+ */
 int main(void)
 {
 	long x, y;
-	
+
 	x = 612852475143;
-	
+
 	for (y = 2; x > y; y++)
 	{
 		while (x % y == 0)
