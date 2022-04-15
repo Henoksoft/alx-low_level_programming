5a89de7cbeebb7334b48c4b2112d61723b3a46e7
henok <henokgirmajifar@yahoo.com>
2022-04-15 04:36:29 -0700

    Task 4 editing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,23 @@
 #include "main.h"
+
 /**
- * print_most_numbers - print numbers
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
  * Return: Always 0.
  */
 void print_most_numbers(void)
 {
+	int a;
 
-int i;
-
-i = 0;
-while (i > 10)
-{
-if (i != 2 && i != 4)
-{
-_putchar (i + '0');
-}
-i++;
-}
-
-_putchar ('\n');
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+				_putchar(a);
+			}
+		}
+	}
+	_putchar('\n');
 }
