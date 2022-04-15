b867a641378f7a51775f47552452508a71f5d180
henok <henokgirmajifar@yahoo.com>
2022-04-15 04:48:57 -0700

    Task 6 editing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,20 @@
 #include "main.h"
+
 /**
- * print_line - draws a straight line in the terminal.
- *@n: print int
+ * print_line - checks for checks for a digit (0 through 9).
+ *@n: n - Variable
  * Return: Always 0.
  */
 void print_line(int n)
 {
-
-int i;
-
-if (n > 0)
-{
-for (i = 0; i < n; i++)
-{
-_putchar (95);
-}
-
-_putchar ('\n');
-}
-
-else
-{
-_putchar ('\n');
-}
-
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar (95);
+		}
+	}
+	_putchar ('\n');
 }
