fe87d0054aa834af5053e97d069260e23248f29b
henok <henokgirmajifar@yahoo.com>
2022-04-22 07:37:27 -0700

    TASK 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * cap_string - string cap
- * @s: cap string
+ * @n: cap string
  *
  * Return: char value
  */
-char *cap_string(char *s)
+char *cap_string(char *n)
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{' '}'};
+	int i, x;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
 
-		while (s[a])
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
 		{
-			i = 0;
-
-			while (i < cspc)
-			{
-				if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-					s[a] -= 32;
-
-				i++;
-			}
-
-			a++;
+			n[i] = n[i] - cap;
 		}
 
-		return (s);
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == separators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
 	}
+	return (n);
+}
