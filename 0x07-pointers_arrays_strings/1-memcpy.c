187f7320d39c7d50eee900c2aaf918902c8c31b6
henok <henokgirmajifar@yahoo.com>
2022-04-26 02:13:04 -0700

    TASK 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  *
  * Return: memory area placed
  */
-char *_memcpy(char *feast, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 	for (a = 0; a < n; a++)
