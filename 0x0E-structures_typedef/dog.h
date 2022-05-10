abbaf3ea4b9d8aba7019eeea5d85a9fe0aa7d5b6
henok <henokgirmajifar@yahoo.com>
2022-05-10 03:23:08 -0700

    TASK 00

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _dog_h_
-#define _dog_h_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - such structure, wow
@@ -7,20 +7,21 @@
  * @age: dog's age
  * @owner: dog's owner
  */
-
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
 
 #endig /* _dog_h_ */
