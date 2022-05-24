c80c76715b301940307d3bb0a8c983b48ab24f56
Henoksoft <henokgirmajifar@yahoo.com>
2022-05-24 13:19:51 -0700

    Task 2 description typo edition

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,8 @@
 #include "lists.h"
 
 /**
- * *add_nodint - Adds a new node at the beginning of a listint_t list.
+ * add_nodeint - Adds a new node at the beginning
+ * of a listint_t list.
  * @head: A pointer to the address of the head
  * of the listint_t list.
  * @n: The integer for the new node to contain.
