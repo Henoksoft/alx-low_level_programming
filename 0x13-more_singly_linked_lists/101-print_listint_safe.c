4f0deb44f9025e267f99308122adebc8b854721d
Henoksoft <henokgirmajifar@yahoo.com>
2022-05-24 12:12:15 -0700

    TASK 12 EDITION

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -83,7 +83,7 @@ size_t print_listint_safe(const listint_t *head)
 		for (index = 0; index < nodes; index++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
-			head - head->next;
+			head = head->next;
 		}
 
 		printf("-> [%p] %d\n", (void *)head, head->n);
